4dbeb9022da5ed058008cf15a9ef96338117c17f
RicardJonathan <ricardjonathan36@gamil.com>
2024-05-17 20:09:35 +0700

    Onbording

--- app/src/main/java/com/example/bulbulapp/Screen.kt
+++ app/src/main/java/com/example/bulbulapp/Screen.kt
@@ -3,4 +3,5 @@ package com.example.bulbulapp
 sealed class Screen(val route: String){
     data object Splash: Screen("splash")
     data object Home: Screen("home")
+    data object Login: Screen("login")
 }
