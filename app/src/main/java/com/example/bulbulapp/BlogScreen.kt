8751e6a21f24e4971c02a6b7dcf000c18eec33be
Ariyanaasyer <asyer.ariyana@gmail.com>
2024-05-25 17:00:54 +0700

    menambahkan filter di Blog Screen

--- app/src/main/java/com/example/bulbulapp/BlogScreen.kt
+++ app/src/main/java/com/example/bulbulapp/BlogScreen.kt
@@ -2,12 +2,29 @@ package com.example.bulbulapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,8 +36,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
-import com.example.bulbulapp.ui.theme.BulBulAppTheme
 
 @Composable
 fun BlogScreen(modifier: Modifier = Modifier) {
@@ -42,21 +57,21 @@ fun BlogScreen(modifier: Modifier = Modifier) {
                     .clip(shape = RoundedCornerShape(112.dp))
                     .background(color = Color(0xffeef1f4))
             ) {
-                IconButton(onClick ={}) {
+                IconButton(onClick = {}) {
                     Icon(
                         imageVector = Icons.Filled.ArrowBack,
                         contentDescription = "Back",
-                        tint = Color.Black,
+                        tint = Color.Gray,
                         modifier = Modifier
                             .align(alignment = Alignment.TopStart)
                             .offset(
                                 x = 10.dp,
                                 y = 10.dp
                             )
-                            .requiredSize(size = 20.dp))
+                            .requiredSize(size = 20.dp)
+                    )
                 }
             }
-
             Spacer(modifier = Modifier.weight(1f))
             Text(
                 text = "Blog",
@@ -68,7 +83,6 @@ fun BlogScreen(modifier: Modifier = Modifier) {
             )
             Spacer(modifier = Modifier.weight(1f))
         }
-
         // Box search bar
         Box(
             modifier = Modifier
@@ -81,16 +95,23 @@ fun BlogScreen(modifier: Modifier = Modifier) {
                 contentDescription = "Blog post image",
                 modifier = Modifier
                     .fillMaxSize()
-                    .clip(RoundedCornerShape(topStart = 5.dp, topEnd = 16.dp))
             )
             //Text  Field search bar
+            //Search bar masih perlu diperbaiki: search bar masih ada bayangan & line di bawah search bar //
             androidx.compose.material.TextField(
                 value = "",
                 onValueChange = {},
-                placeholder = { Text("Hewan Bahagia, Keluarga pun senang!", fontSize = 14.sp) },
+                placeholder = {
+                    Text(
+                        "Hewan Bahagia, Keluarga pun senang!",
+                        color = Color.Gray,
+                        fontSize = 14.sp,
+                    )
+                },
                 leadingIcon = {
                     Icon(
                         imageVector = Icons.Filled.Search,
+                        tint = Color.Gray,
                         contentDescription = "Search icon"
                     )
                 },
@@ -99,17 +120,128 @@ fun BlogScreen(modifier: Modifier = Modifier) {
                     .fillMaxWidth(1f)
                     .align(Alignment.BottomCenter)
                     .padding(15.dp)
-                    .background(Color.White, shape = RoundedCornerShape(10.dp)),
-
-
-                )
+                    .background(Color.White, shape = RoundedCornerShape(30.dp)),
+            )
         }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                Arrangement.SpaceEvenly
+            ) {
+                Spacer(modifier = Modifier.height(12.dp))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(3.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 10.dp) // Menambahkan padding horizontal
+                        .padding(bottom = 650.dp) // Menambahkan padding bottom agar tidak terlalu bawah
+                        .align(Alignment.CenterHorizontally)
+                ) {
+                    val primaryColor = Color(0xFFFF8066)
+                    Button(
+                        onClick = { TODO() },
+                        modifier = Modifier
+                            .height(32.dp)
+                            .width(90.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            primaryColor
+                        ),
+                        shape = RoundedCornerShape(5.dp),
+                    ) {
+                        Text(
+                            text = "Semua",
+                            color = Color.White,
+                            fontSize = 10.sp
+                        )
+                    }
+                    val secondaryColor = Color(0xF2F2F3F7)
+                    Button(
+                        onClick = { TODO() },
+                        modifier = Modifier
+                            .height(32.dp)
+                            .width(90.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            secondaryColor
+                        ),
+                        shape = RoundedCornerShape(5.dp),
+                    ) {
+                        Text(
+                            text = "Anjing",
+                            color = primaryColor,
+                            fontSize = 10.sp,
+                            textAlign = TextAlign.Justify,
+                        )
+                    }
+                    Button(
+                        onClick = { TODO() },
+                        modifier = Modifier
+                            .height(32.dp)
+                            .width(90.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            secondaryColor
+                        ),
+                        shape = RoundedCornerShape(5.dp),
+                    ) {
+                        Text(
+                            text = "Kucing",
+                            color = primaryColor,
+                            fontSize = 10.sp
+                        )
+                    }
+                    Button(
+                        onClick = { TODO() },
+                        modifier = Modifier
+                            .height(32.dp)
+                            .width(95.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            secondaryColor
+                        ),
+                        shape = RoundedCornerShape(5.dp),
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Center,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.FilterList, // Menggunakan ikon filter bawaan, boleh diubah
+                                    contentDescription = "Filter",
+                                    tint = primaryColor,
+                                    modifier = Modifier
+                                        .height(12.dp)
+                                        .width(12.dp)
+                                        .align(Alignment.CenterVertically)
+                                )
+                                Spacer(modifier = Modifier.width(1.dp)) // Spasi antara ikon dan teks
+                                Text(
+                                    text = "Filter",
+                                    modifier = Modifier
+                                        .align(Alignment.CenterVertically),
+                                    color = primaryColor,
+                                    fontSize = 10.sp,
 
+
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun BlogScreenPreview() {
     BlogScreen()
--- app/src/main/java/com/example/bulbulapp/NavigationApp.kt
+++ app/src/main/java/com/example/bulbulapp/NavigationApp.kt
@@ -5,8 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.bulbulapp.profile.LogoutOverlayScreen
-import com.example.bulbulapp.profile.ProfileScreen
 
 @Composable
 fun NavigationApp() {
