976b782b8f955042d969f176f6e3a191aacdd4bd
Albert Billy <albertbilly04@gmail.com>
2024-06-19 21:25:01 +0700

    Layanan

--- app/src/main/java/com/example/bulbulapp/screen/ServiceListScreen.kt
+++ app/src/main/java/com/example/bulbulapp/screen/ServiceListScreen.kt
@@ -1,23 +1,20 @@
-
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,20 +64,127 @@ fun ServiceListScreen(navController: NavController) {
             )
         },
         content = { paddingValues ->
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
+            Column(
                 modifier = Modifier
+                    .fillMaxSize()
                     .padding(paddingValues)
-                    .padding(horizontal = 16.dp, vertical = 8.dp) // Apply padding here
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
             ) {
-                items(DummyData.listLayanan) { layanan ->
-                    CardlayananItem(
-                        layanan = layanan,
-                        onItemClicked = { clickedLayanan ->
-                            // Handle item click here
+                var searchText by remember { mutableStateOf("") } // State to hold the text input
 
-                        }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp) // Adjust the height to fit the search bar and filter
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.bghome), // Your background image resource
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
                     )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        TextField(
+                            value = searchText,
+                            onValueChange = { newText -> searchText = newText },
+                            placeholder = { Text("Temukan Layanan Terbaik") },
+                            leadingIcon = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.search1),
+                                    contentDescription = "Search Icon"
+                                )
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(12.dp))
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 8.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.location1),
+                                    contentDescription = "Location Icon",
+                                    tint = Color(0xFF000000)
+                                )
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(
+                                    text = "Surabaya, Jawa Timur",
+                                    color = Color(0xff6d6f77),
+                                    style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Medium)
+                                )
+                            }
+                            TextButton(
+                                onClick = { /* TODO: Add filter logic */ },
+                                colors = ButtonDefaults.textButtonColors(
+                                    contentColor = Color(0xff6d6f77)
+                                )
+                            ) {
+                                Text("Filter")
+                            }
+                        }
+                    }
+                }
+
+                // Filter Buttons
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(
+                        onClick = { /* TODO: Add "Semua" filter logic */ },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xffffa694)),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text("Semua")
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(
+                        onClick = { /* TODO: Add "Grooming" filter logic */ },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xffffa694)),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text("Grooming")
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(
+                        onClick = { /* TODO: Add "Pet Clinic" filter logic */ },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xffffa694)),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text("Pet Clinic")
+                    }
+                }
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp) // Apply padding here
+                ) {
+                    items(DummyData.listLayanan) { layanan ->
+                        CardlayananItem(
+                            layanan = layanan,
+                            onItemClicked = { clickedLayanan ->
+                                // Handle item click here
+                            }
+                        )
+                    }
                 }
             }
         }
